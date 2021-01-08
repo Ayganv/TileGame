587ab537383eb57f161f14474fcff88dddeec66e
Virginia Baroni <virg.baroni@gmail.com>
2021-01-08 02:53:30 +0100

    All UI is ready and working, need only switch tiles and save/load.

--- Assets/ScriptableButtons/ButtonTypes/TileTypes.cs
+++ Assets/ScriptableButtons/ButtonTypes/TileTypes.cs
@@ -8,10 +8,12 @@ public class TileTypes : ScriptableObject
     [SerializeField] public int tileID;
     [SerializeField] public string tileName;
     [SerializeField] public Color color;
+    [SerializeField] public Color h_color;
     
     #region getters and setters
     public int TileID { get => tileID; set => tileID = value; }
     public string TileName { get => tileName; set => tileName = value; }
-    public Color Titlecolor { get => color; set => color = value; }
+    public Color Tilecolor { get => color; set => color = value; }
+    public Color HighlightedTilecolor { get => h_color; set => h_color = value; }
     #endregion
 }
--- Assets/Scripts/TileScript.cs
+++ Assets/Scripts/TileScript.cs
@@ -5,18 +5,80 @@ using UnityEngine.UI;
 
 public class TileScript : MonoBehaviour
 {
-    [SerializeField] public TileTypes bluetile;
     [SerializeField] public TileTypes greentile;
+    [SerializeField] public TileTypes bluetile;
+    [SerializeField] public TileTypes yellowtile;
+    [SerializeField] public TileTypes redtile;
+    [SerializeField] public TileTypes browntile;
+    public Text TileName;
     //[SerializeField] public TileTypes extratile;
+    [SerializeField] public int tileID;
 
+    /*  colors.highlightedColor = greentile.h_color;
+        colors.normalColor = greentile.color;
+        colors.pressedColor = greentile.color;
+     */
     void Start()
     {
         var colors = GetComponent<Button>().colors;
-        colors.normalColor = greentile.color;
-        GetComponent<Button>().colors = colors;
+
+        if (tileID == 0)
+        {
+            colors.highlightedColor = greentile.h_color;
+            colors.normalColor = greentile.color;
+            colors.pressedColor = greentile.color;
+            colors.selectedColor = greentile.color;
+            GetComponent<Button>().colors = colors;
+            TileName.text = greentile.tileName;
+        } else if (tileID == 1)
+        {
+            colors.highlightedColor = bluetile.h_color;
+            colors.normalColor = bluetile.color;
+            colors.pressedColor = bluetile.color;
+            colors.selectedColor = bluetile.color;
+            GetComponent<Button>().colors = colors;
+            TileName.text = bluetile.tileName;
+        } else if (tileID == 2)
+        {
+            colors.highlightedColor = yellowtile.h_color;
+            colors.normalColor = yellowtile.color;
+            colors.pressedColor = yellowtile.color;
+            colors.selectedColor = yellowtile.color;
+            GetComponent<Button>().colors = colors;
+            TileName.text = yellowtile.tileName;
+        } else if (tileID == 3)
+        {
+            colors.highlightedColor = redtile.h_color;
+            colors.normalColor = redtile.color;
+            colors.pressedColor = redtile.color;
+            colors.selectedColor = redtile.color;
+            GetComponent<Button>().colors = colors;
+            TileName.text = redtile.tileName;
+        } else if (tileID == 4)
+        {
+            colors.highlightedColor = browntile.h_color;
+            colors.normalColor = browntile.color;
+            colors.pressedColor = browntile.color;
+            colors.selectedColor = browntile.color;
+            GetComponent<Button>().colors = colors;
+            TileName.text = browntile.tileName;
+        }
+        /*else if (tileID == 5) {
+            colors.highlightedColor = extratile.h_color;
+            colors.normalColor = extratile.color;
+            colors.pressedColor = extratile.color;
+            colors.selectedColor = extratile.color;
+            GetComponent<Button>().colors = colors;
+            TileName.text = extratile.tileName;
+        }*/
     }
 
-    public void ChangeTile()
+    public void EditExistingTile()
+    {
+        
+    }
+
+    public void ReplaceTile()
     {
         //
     }
